ed2335da047d37f40cd03c9a30d759f4be979c31
chengtianhua <1057691751@qq.com>
2021-06-15 21:22:38 +0800

    BsRangePicker 防止onChange覆盖

--- packages/runtime/src/components/Base/BsRangePicker/index.tsx
+++ packages/runtime/src/components/Base/BsRangePicker/index.tsx
@@ -1,24 +1,24 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState, useEffect } from "react";
-import { Form, Input, DatePickerProps } from "antd";
-import moment, { Moment } from "moment";
+import React, { useState, useEffect } from 'react'
+import { Form, Input, DatePickerProps } from 'antd'
+import moment, { Moment } from 'moment'
 import {
   FormComponentProps,
   FormComponent,
-} from "@scboson/sc-element/lib/c-form";
-import { RangeValue } from "rc-picker/es/interface";
+} from '@scboson/sc-element/lib/c-form'
+import { RangeValue } from 'rc-picker/es/interface'
 
-import RangePicker from "./RangePicker";
+import RangePicker from './RangePicker'
 
 export interface ScRangePickerProps extends FormComponentProps {
-  format?: string;
-  returnType?: "string" | "date";
-  startTimeFiled?: string;
-  endTimeFiled?: string;
-  showTime?: Object | boolean;
-  rulesRequire?: boolean;
-  disabled?: boolean;
+  format?: string
+  returnType?: 'string' | 'date'
+  startTimeFiled?: string
+  endTimeFiled?: string
+  showTime?: Object | boolean
+  rulesRequire?: boolean
+  disabled?: boolean
 }
 
 const ScRangePicker: FormComponent<DatePickerProps & ScRangePickerProps> = (
@@ -26,116 +26,115 @@ const ScRangePicker: FormComponent<DatePickerProps & ScRangePickerProps> = (
 ) => {
   const {
     format,
-    returnType = "string",
-    startTimeFiled = "startTime",
-    endTimeFiled = "endTime",
+    returnType = 'string',
+    startTimeFiled = 'startTime',
+    endTimeFiled = 'endTime',
     showTime = true,
     form,
     readonly,
     initialValues,
     rulesRequire = false,
     ...resProps
-  } = props;
+  } = props
 
-  const cformat = format || (showTime ? "YYYY-MM-DD HH:mm:ss" : "YYYY-MM-DD");
-  const [currentValue, setCurrentValue] = useState<any>();
+  const cformat = format || (showTime ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD')
+  const [currentValue, setCurrentValue] = useState<any>()
 
   const formatValue = (): void => {
-    const startTime = form?.getFieldValue(startTimeFiled);
-    const endTime = form?.getFieldValue(endTimeFiled);
+    const startTime = form?.getFieldValue(startTimeFiled)
+    const endTime = form?.getFieldValue(endTimeFiled)
     if (startTime && endTime) {
       form?.setFieldsValue({
         [`${startTimeFiled}_${endTimeFiled}`]: [
           moment.utc(startTime, cformat),
           moment.utc(endTime, cformat),
         ],
-      });
+      })
       setCurrentValue([
         moment.utc(startTime, cformat),
         moment.utc(endTime, cformat),
-      ]);
+      ])
     } else {
       form?.setFieldsValue({
         [`${startTimeFiled}_${endTimeFiled}`]: [],
-      });
-      setCurrentValue([]);
+      })
+      setCurrentValue([])
     }
-  };
+  }
 
   useEffect(() => {
-    formatValue();
-  }, [form?.getFieldValue(startTimeFiled)]);
+    formatValue()
+  }, [form?.getFieldValue(startTimeFiled)])
 
   const handleChange = (
     dates: RangeValue<Moment>,
     dateStrings: [string, string]
   ) => {
     const _dates: {
-      [key: string]: string | Date;
-    } = {};
+      [key: string]: string | Date
+    } = {}
     const _dateStrings: {
-      [key: string]: any;
-    } = {};
-    let temStartTimeFiled, temEndTimeFiled;
-    let temData = _dateStrings;
+      [key: string]: any
+    } = {}
+    let temStartTimeFiled, temEndTimeFiled
+    let temData = _dateStrings
     if (Array.isArray(startTimeFiled)) {
-      temStartTimeFiled = startTimeFiled[startTimeFiled.length - 1];
-      temEndTimeFiled = endTimeFiled[endTimeFiled.length - 1];
-     
-      startTimeFiled.forEach((item,i) => {
-        if (i<startTimeFiled.length- 1){
-          temData[item] = {};
-          temData = temData[item];
+      temStartTimeFiled = startTimeFiled[startTimeFiled.length - 1]
+      temEndTimeFiled = endTimeFiled[endTimeFiled.length - 1]
+
+      startTimeFiled.forEach((item, i) => {
+        if (i < startTimeFiled.length - 1) {
+          temData[item] = {}
+          temData = temData[item]
         }
-      
-      });
+      })
     } else {
-      temStartTimeFiled = startTimeFiled;
-      temEndTimeFiled = endTimeFiled;
+      temStartTimeFiled = startTimeFiled
+      temEndTimeFiled = endTimeFiled
     }
     if (Array.isArray(dates)) {
-      if (returnType === "date") {
-        _dates[`${temStartTimeFiled}`] = moment.utc(dates[0]).toDate();
-        _dates[`${temEndTimeFiled}`] = moment.utc(dates[1]).toDate();
+      if (returnType === 'date') {
+        _dates[`${temStartTimeFiled}`] = moment.utc(dates[0]).toDate()
+        _dates[`${temEndTimeFiled}`] = moment.utc(dates[1]).toDate()
       }
-      if (returnType === "string") {
-        _dates[`${temStartTimeFiled}`] = dateStrings[0];
-        _dates[`${temEndTimeFiled}`] = dateStrings[1];
+      if (returnType === 'string') {
+        _dates[`${temStartTimeFiled}`] = dateStrings[0]
+        _dates[`${temEndTimeFiled}`] = dateStrings[1]
       }
     }
-    temData[`${temStartTimeFiled}`] = dateStrings[0];
-    temData[`${temEndTimeFiled}`] = dateStrings[1];
-    form?.setFieldsValue(_dateStrings);
-    formatValue();
-  };
+    temData[`${temStartTimeFiled}`] = dateStrings[0]
+    temData[`${temEndTimeFiled}`] = dateStrings[1]
+    form?.setFieldsValue(_dateStrings)
+    formatValue()
+  }
 
   const formatValueView = () => {
-    const startTime = form?.getFieldValue(startTimeFiled);
-    const endTime = form?.getFieldValue(endTimeFiled);
+    const startTime = form?.getFieldValue(startTimeFiled)
+    const endTime = form?.getFieldValue(endTimeFiled)
 
     if (startTime && endTime) {
-      const startTimeView = moment.utc(startTime).format(cformat);
-      const endTimeView = moment.utc(endTime).format(cformat);
+      const startTimeView = moment.utc(startTime).format(cformat)
+      const endTimeView = moment.utc(endTime).format(cformat)
       return (
         <div>
           {startTimeView} - {endTimeView}
         </div>
-      );
+      )
     } else {
-      return <div></div>;
+      return <div></div>
     }
-  };
+  }
 
   if (readonly) {
-    return formatValueView();
+    return formatValueView()
   } else {
     return (
       <Form.Item noStyle>
-        <div style={{ display: "none" }}>
-          <Form.Item name={startTimeFiled} style={{ display: "none" }}>
+        <div style={{ display: 'none' }}>
+          <Form.Item name={startTimeFiled} style={{ display: 'none' }}>
             <Input></Input>
           </Form.Item>
-          <Form.Item name={endTimeFiled} style={{ display: "none" }}>
+          <Form.Item name={endTimeFiled} style={{ display: 'none' }}>
             <Input></Input>
           </Form.Item>
         </div>
@@ -147,24 +146,24 @@ const ScRangePicker: FormComponent<DatePickerProps & ScRangePickerProps> = (
               ? [
                   {
                     required: true,
-                    message: "请选择",
+                    message: '请选择',
                   },
                 ]
               : undefined
           }
         >
           <RangePicker
+            {...resProps}
             format={cformat}
             onChange={handleChange}
-            {...resProps}
             showTime={showTime}
           />
         </Form.Item>
       </Form.Item>
-    );
+    )
   }
-};
+}
 
-ScRangePicker.customView = true;
+ScRangePicker.customView = true
 
-export default ScRangePicker;
+export default ScRangePicker
