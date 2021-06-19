649b3a9d4b87516ccf51eb23816f0374874e08f2
chengtianhua <1057691751@qq.com>
2021-06-19 15:52:04 +0800

    图片加名称的组件添加备注信息

--- packages/runtime/src/components/Base/FieldImageName/index.tsx
+++ packages/runtime/src/components/Base/FieldImageName/index.tsx
@@ -6,16 +6,22 @@ import { BsTableComponentProps } from '../BsTable'
 type FieldImageNameProps = BsTableComponentProps & {
   fieldName: string
   fieldImage: string
+  fildDesc?: string
 }
 
 const FieldImageName: React.FC<FieldImageNameProps> = (props) => {
-  const { fieldImage, fieldName, rowData } = props
+  const { fieldImage, fieldName, rowData, fildDesc } = props
   return (
     <div className={styles['bg-field-line']}>
       <div className={styles['bg-field-image']}>
         <BsImg src={rowData[fieldImage]}></BsImg>
       </div>
-      <div className={styles['bg-field-value']}>{rowData[fieldName]}</div>
+      <div className={styles['bg-field-value']}>
+        <span>{rowData[fieldName]}</span>
+        {fildDesc ? (
+          <span className={styles['bg-field-desc']}>{fildDesc}</span>
+        ) : null}
+      </div>
     </div>
   )
 }
