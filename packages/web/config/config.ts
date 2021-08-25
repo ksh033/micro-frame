f4712029bf1d95444c7cd8f9ed9db97e623e3381
skimq <2072616987@qq.com>
2021-08-25 10:36:16 +0800

    更改目录

--- packages/web/config/config.ts
+++ packages/web/config/config.ts
@@ -16,7 +16,7 @@ export const EVN_CONFIG = {
   dev: {
     imgUrl: 'http://test.bogengkeji.com/images',
     apiUrl: '/webapi-dev',
-    masterUrl: 'http://172.18.164.54:3000/',
+    masterUrl: 'http://172.18.164.54/',
   },
   pro: {
     imgUrl: 'https://images.bogengkeji.com/',
