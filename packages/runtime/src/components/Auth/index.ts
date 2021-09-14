a58a44a28b0ce0c63e7bb3e4d55c8d32b29dbf0e
skimq <2072616987@qq.com>
2021-09-14 22:04:20 +0800

    修改

--- packages/runtime/src/components/Auth/index.ts
+++ packages/runtime/src/components/Auth/index.ts
@@ -60,7 +60,7 @@ const addCookie= (objName, objValue, objHours)=> {
 }
 
 //读Cookie
-const getCookie=(objName) {//获取指定名称的cookie的值
+const getCookie=(objName)=> {//获取指定名称的cookie的值
   var arrStr = document.cookie.split("; ");
   for (var i = 0; i < arrStr.length; i++) {
       var temp = arrStr[i].split("=");
