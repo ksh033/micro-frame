60ec00c25cc99a5e417a10243b3fd057e4539146
chengtianhua <1057691751@qq.com>
2022-01-21 10:25:22 +0800

    选择商品品目接口处理

--- packages/runtime/src/components/Bus/GoodsCatalog/GoodsCatalogSelect/index.tsx
+++ packages/runtime/src/components/Bus/GoodsCatalog/GoodsCatalogSelect/index.tsx
@@ -1,39 +1,32 @@
-import React from 'react';
-import { ScTreeSelect } from '@scboson/sc-element';
+import React from 'react'
+import { ScTreeSelect } from '@scboson/sc-element'
 
-import type { ScTreeSelectProps } from '@scboson/sc-element/es/sc-tree-select';
+import type { ScTreeSelectProps } from '@scboson/sc-element/es/sc-tree-select'
 
 import { uesRequest } from '../../../../utils/api'
 
-
-
 const GoodsCatalogTree: React.FC<ScTreeSelectProps> = (props) => {
-  const api = uesRequest('mall', 'goodsCatalogList');
+  const api = uesRequest('mall', 'goodsCatalogList')
   const loadDataPramsFormat = (item: any) => {
     return {
       parentId: item.goodsCatalogId,
-    };
-  };
-  
+    }
+  }
+
   return (
-   
-      <ScTreeSelect
+    <ScTreeSelect
+      placeholder={'商品品目'}
+      textField="catalogName"
+      valueField="goodsCatalogId"
       {...props}
-        placeholder={'商品品目'}
-        //@ts-ignore
-        async={true}
-        loadDataPramsFormat={loadDataPramsFormat}
-        autoload={true}
-        request={api.run}
-        params={{ parentId: '0' }}
-        textField="catalogName"
-        valueField="goodsCatalogId"
-      
-      />
-  
-  );
-};
+      //@ts-ignore
+      async={true}
+      loadDataPramsFormat={loadDataPramsFormat}
+      autoload={true}
+      request={api.run}
+      params={{ parentId: '0' }}
+    />
+  )
+}
 
-
-
-export default GoodsCatalogTree;
+export default GoodsCatalogTree
