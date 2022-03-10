f469f0316273d62b43d45fda3074ef7169cd442a
skimq <2072616987@qq.com>
2022-03-10 22:24:40 +0800

    允许重发

--- scripts/release.js
+++ scripts/release.js
@@ -159,7 +159,7 @@ async function release() {
             isNext ? "with next tag" : ""
           }`
         );
-        const cliArgs = isNext ? ["publish", "--tag", "next"] : ["publish"];
+        const cliArgs = isNext ? ["publish", "--tag", "beta",'--dry-run'] : ["publish"];
         const { stdout } = execa.sync("npm", cliArgs, {
           cwd: pkgPath,
         });
