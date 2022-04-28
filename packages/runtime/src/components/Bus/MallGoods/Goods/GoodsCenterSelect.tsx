cdb0ab7125d097d3532876e7144967c1aad00cb9
ksh033 <94456671@qq.com>
2022-04-28 18:20:26 +0800

    修改

--- packages/runtime/src/components/Bus/MallGoods/Goods/GoodsCenterSelect.tsx
+++ packages/runtime/src/components/Bus/MallGoods/Goods/GoodsCenterSelect.tsx
@@ -1,176 +1,250 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import React, { useMemo, useState, useRef, PropsWithRef } from 'react';
+import React, { useMemo, useState, useRef, PropsWithRef } from "react";
 
-import type { ProColumn } from '@scboson/sc-schema/es/interface';
-import BsTable  from '../../../Base/BsTable';
+import type { ProColumn } from "@scboson/sc-schema/es/interface";
+import BsTable from "../../../Base/BsTable";
 
-import {ScCard} from '@scboson/sc-layout'
-import GoodsCatalogTree from '../../../Bus/MallGoods/Catalog/CatalogTree';
-import { WithSelectTable, WithSelectTableProps, WithTableProps } from '../../../WithComponent';
-import GoodsCenterTable from './GoodsCenterTable'
-import { CModal } from '@scboson/sc-element';
-import { Button } from 'antd';
-import { ButtonProps } from 'antd/es/button/button'
-import { CModalDialogProps } from '@scboson/sc-element/es/c-modal';
-import type { FormComponent } from '@scboson/sc-element/es/c-form';
+import { ScCard } from "@scboson/sc-layout";
+import GoodsCatalogTree from "../../../Bus/MallGoods/Catalog/CatalogTree";
+import {
+  WithSelectTable,
+  WithSelectTableProps,
+  WithTableProps,
+} from "../../../WithComponent";
+import GoodsCenterTable from "./GoodsCenterTable";
+import { CModal } from "@scboson/sc-element";
+import { Button } from "antd";
+import { ButtonProps } from "antd/es/button/button";
+import { CModalDialogProps } from "@scboson/sc-element/es/c-modal";
+import type { FormComponent } from "@scboson/sc-element/es/c-form";
 
-
-export type GoodsTransferProps = WithSelectTableProps & WithTableProps & {
+export type GoodsTransferProps = WithSelectTableProps &
+  WithTableProps & {
     extraColumns?: ProColumn[];
     request?: (params: any) => Promise<any>; // 请求数据的远程方法
     params?: any;
     rowKey?: string;
-    onSubmitGoods?: (goodsList: { goodsId: string, goodsName: string, [key: string]: any }[], buttonIndex?: string, clearRows?: () => void) => void,
+    onSubmitGoods?: (
+      goodsList: { goodsId: string; goodsName: string; [key: string]: any }[],
+      buttonIndex?: string,
+      clearRows?: () => void
+    ) => void;
     tableProps?: any;
     buttonProps?: ButtonProps & { text: string };
-    modalProps?: CModalDialogProps
-    header?: React.ReactNode
+    modalProps?: CModalDialogProps;
+    header?: React.ReactNode;
 
-    customRef?:React.RefObject<any>
+    customRef?: React.RefObject<any>;
+  };
 
+const DlgContent = (porps: GoodsTransferProps) => {
+  const [rightSelectedRows, setRightSelectRows] = useState<any[]>();
+  const {
+    selectionType,
+    extraColumns,
+    request,
+    rowKey = "goodsId",
+    params,
+    onTabelRow,
+    header,
+    customRef,
+  } = porps;
+  const [catalogId, setCatalogId] = useState<string>("");
+  const tableParams = useMemo(() => {
+    let newPrams = {
+      catalogId,
+      ...params,
+    };
+    return newPrams;
+  }, [catalogId, JSON.stringify(params)]);
 
-};
-
-const DlgContent = (porps:GoodsTransferProps) => {
-    const [rightSelectedRows, setRightSelectRows] = useState<any[]>()
-    const { selectionType, extraColumns, request, rowKey = "goodsId", params, onTabelRow, header, customRef } = porps
-    const [catalogId, setCatalogId] = useState<string>("")
-    const tableParams = useMemo(() => {
-        let newPrams = {
-            catalogId,
-            ...params
-        };
-        return newPrams;
-    }, [catalogId, JSON.stringify(params)]);
-
-    const clearSelectRow = () => {
-        setRightSelectRows([])
-        onTabelRow && onTabelRow([], [])
+  const clearSelectRow = () => {
+    setRightSelectRows([]);
+    onTabelRow && onTabelRow([], []);
+  };
+  if (customRef !== null && customRef !== undefined) {
+    if (typeof customRef === "object") {
+      customRef.current.clearSelectRow = clearSelectRow;
     }
-    if (customRef !== null && customRef !== undefined) {
-        if (typeof customRef === "object") {
-            customRef.current.clearSelectRow = clearSelectRow
-        }
-    }
+  }
 
-    return (<>{header}<ScCard style={{ maxWidth: '1150px', height: '460px' }} bodyStyle={{ margin: '0px', padding: '0px', overflow: 'hidden' }} gutter={4}>
+  return (
+    <>
+      {header}
+      <ScCard
+        bodyStyle={{
+          margin: "0px",
+          padding: "0px",
+          maxWidth: "1150px",
+          height: "580px",
+          overflow: "auto",
+        }}
+        gutter={4}
+      >
         <ScCard
-            colSpan="170px"
-            // style={{ height: '100%' }}
-            bodyStyle={{ padding: '0px', overflow: 'auto' }}
+          colSpan="170px"
+          // style={{ height: '100%' }}
+          bodyStyle={{ padding: "0px", overflow: "auto" }}
         >
-            <GoodsCatalogTree
-                height={490}
-                onSelect={(selectedKeys) => {
-                    let [key] = selectedKeys
-                    if (key) {
-                        if (key == "0") {
-                            key = "";
-                        } else {
-                            key = key + ""
-                        }
-                        setCatalogId(key)
-                    }
-
-                }}
-                loadedKeys={[]}
-                autoload={false}
-            />
+          <GoodsCatalogTree
+            height={490}
+            onSelect={(selectedKeys) => {
+              let [key] = selectedKeys;
+              if (key) {
+                if (key == "0") {
+                  key = "";
+                } else {
+                  key = key + "";
+                }
+                setCatalogId(key);
+              }
+            }}
+            loadedKeys={[]}
+            autoload={false}
+          />
         </ScCard>
-        <ScCard bodyStyle={{ padding: '0px' }} style={{ height: '100%' }}>
-            <GoodsCenterTable size="small" params={tableParams} selectionType={selectionType} selectedRows={rightSelectedRows} extraColumns={extraColumns}
-                onTabelRow={(keys, rows) => {
-                    onTabelRow && onTabelRow(keys, rows)
-                    setRightSelectRows(rows)
-                }}
-                rowKey={rowKey} request={request}></GoodsCenterTable>
-
+        <ScCard bodyStyle={{ padding: "0px" }} style={{ height: "100%" }}>
+          <GoodsCenterTable
+            size="small"
+            params={tableParams}
+            selectionType={selectionType}
+            selectedRows={rightSelectedRows}
+            extraColumns={extraColumns}
+            onTabelRow={(keys, rows) => {
+              onTabelRow && onTabelRow(keys, rows);
+              setRightSelectRows(rows);
+            }}
+            rowKey={rowKey}
+            request={request}
+          ></GoodsCenterTable>
         </ScCard>
         <ScCard
-            colSpan="300px"
-            style={{ height: '100%' }}
-            bodyStyle={{ padding: '0px' }}
+          colSpan="280px"
+          style={{ height: "100%" }}
+          bodyStyle={{ padding: "0px" }}
         >
-            <BsTable rowKey={rowKey} scroll={{ y: 360 }} pagination={false} title={() => {
-                return <div style={{ marginTop: '10px' }}>已选商品</div>
-            }} dataSource={rightSelectedRows} size="small" columns={[
-                { title: '商品', dataIndex: 'goodsName' },
-                {
-                    title: '操作',
-                    render: (v, record) => {
-                        const id = record[rowKey]
-                        return <Button type='link' onClick={() => {
-                            const rows = rightSelectedRows ? rightSelectedRows?.filter((item: any) => (item[rowKey] != id)) : []
-                            setRightSelectRows(rows)
-                            const keys = rows ? rows?.map((item: any) => item[rowKey]) : []
-                            onTabelRow && onTabelRow(keys, rows)
-                        }
-
-                        }>删除</Button>
-                    },
-                    width: '80px',
+          <BsTable
+            rowKey={rowKey}
+            scroll={{ y: 420 }}
+            pagination={false}
+            title={() => {
+              return <div style={{ marginTop: "10px" }}>已选商品</div>;
+            }}
+            dataSource={rightSelectedRows}
+            size="small"
+            columns={[
+              {
+                title: "商品",
+                dataIndex: "goodsName",
+                width: 160,
+              },
+              {
+                title: "操作",
+                width: 60,
+                render: (v, record) => {
+                  const id = record[rowKey];
+                  return (
+                    <Button
+                      type="link"
+                      onClick={() => {
+                        const rows = rightSelectedRows
+                          ? rightSelectedRows?.filter(
+                              (item: any) => item[rowKey] != id
+                            )
+                          : [];
+                        setRightSelectRows(rows);
+                        const keys = rows
+                          ? rows?.map((item: any) => item[rowKey])
+                          : [];
+                        onTabelRow && onTabelRow(keys, rows);
+                      }}
+                    >
+                      删除
+                    </Button>
+                  );
                 },
-            ]}>
-            </BsTable>
-
+              },
+            ]}
+          ></BsTable>
         </ScCard>
-    </ScCard></>)
-}
+      </ScCard>
+    </>
+  );
+};
 /**
  * 通用商品选择,请求req外部定义
- * @param props 
- * @returns 
+ *
+ * @param props
+ * @returns
  */
 const GoodsCenterSelect: React.FC<GoodsTransferProps> = (props) => {
-    const { onOk, onSubmitGoods, buttonProps = { text: '新增', type: 'primary' }, modalProps, ...restProps } = props;
-    const { text, ...otherProps } = buttonProps;
+  const {
+    onOk,
+    onSubmitGoods,
+    buttonProps = { text: "新增", type: "primary" },
+    modalProps,
+    ...restProps
+  } = props;
+  const { text, ...otherProps } = buttonProps;
 
-    const ref = useRef<any>({})
-    //const {customToolbar,...restModalProps}=modalProps;
+  const ref = useRef<any>({});
+  //const {customToolbar,...restModalProps}=modalProps;
 
-    const customOnOk = (buttonIndex?: string) => {
-        const selectData = onOk && onOk();
-        let result = onSubmitGoods && onSubmitGoods(selectData, buttonIndex, ref?.current?.clearSelectRow)
-        return result
-    }
-    const toolbar: any = modalProps?.customToolbar ? modalProps?.customToolbar.map(({ onClick, ...restProps }, index) => ({
+  const customOnOk = (buttonIndex?: string) => {
+    const selectData = onOk && onOk();
+    let result =
+      onSubmitGoods &&
+      onSubmitGoods(selectData, buttonIndex, ref?.current?.clearSelectRow);
+    return result;
+  };
+  const toolbar: any = modalProps?.customToolbar
+    ? modalProps?.customToolbar.map(({ onClick, ...restProps }, index) => ({
         onClick: () => {
-            onClick && onClick()
-            return customOnOk("button" + index)
-        }, ...restProps
-    })) : undefined
-    const showDlg = () => {
-        CModal.show({
-            title: '选择商品',
-            content: <DlgContent {...restProps} customRef={ref} />,
-            onOk: () => {
-                return customOnOk()
-            }
-            , ...modalProps,
-            customToolbar: toolbar
-        })
-    }
-    return (
-        <Button onClick={showDlg}
-            {...otherProps} >{text}</Button>
-    );
+          onClick && onClick();
+          return customOnOk("button" + index);
+        },
+        ...restProps,
+      }))
+    : undefined;
+  const showDlg = () => {
+    CModal.show({
+      title: "选择商品",
+      content: <DlgContent {...restProps} customRef={ref} />,
+      onOk: () => {
+        return customOnOk();
+      },
+      ...modalProps,
+      customToolbar: toolbar,
+    });
+  };
+  return (
+    <Button onClick={showDlg} {...otherProps}>
+      {text}
+    </Button>
+  );
 };
 
-/**
- * 商品弹出窗口选择
- */
+/** 商品弹出窗口选择 */
 
-const GoodModalSelect:FormComponent<GoodsTransferProps>= WithSelectTable<GoodsTransferProps>(GoodsCenterSelect, true, {
+const GoodModalSelect: FormComponent<GoodsTransferProps> =
+  WithSelectTable<GoodsTransferProps>(GoodsCenterSelect, true, {
     normalize: (data: any[]) => {
-        if (data)
-            return data.map(({ goodsId, deptGoodsId, goodsName }) => ({ goodsId, deptGoodsId, goodsName }))
-        return []
+      if (data)
+        return data.map(({ goodsId, deptGoodsId, goodsName }) => ({
+          goodsId,
+          deptGoodsId,
+          goodsName,
+        }));
+      return [];
     },
     getValueProps: (data: any[]) => {
-
-        if (data)
-            return data.map(({ goodsId, deptGoodsId, goodsName }) => ({ goodsId, deptGoodsId, goodsName }))
-        return []
-    }
-})
- export default GoodModalSelect;
+      if (data)
+        return data.map(({ goodsId, deptGoodsId, goodsName }) => ({
+          goodsId,
+          deptGoodsId,
+          goodsName,
+        }));
+      return [];
+    },
+  });
+export default GoodModalSelect;
--- packages/runtime/src/components/Bus/MallGoods/Goods/GoodsCenterTable.tsx
+++ packages/runtime/src/components/Bus/MallGoods/Goods/GoodsCenterTable.tsx
@@ -1,57 +1,51 @@
-
-
-import list from './list';
-import type { PageConfig } from '@scboson/sc-schema';
-import { WithTable, WithTableProps } from  '../../../WithComponent';
+import list from "./list";
+import type { PageConfig } from "@scboson/sc-schema";
+import { WithTable, WithTableProps } from "../../../WithComponent";
 const pageConfig: PageConfig = {
-    service: {},
-    ...list,
+  service: {},
+  ...list,
 };
 
 //const defaultReq = getServiceApi('pageconfig', 'queryPage');
 
-
 /**
- * 
  * @param props 通用商品选择表格
- * @returns 
+ * @returns
  */
 const GoodsCenterTable: React.FC<WithTableProps> = (props: any) => {
-    const { children } = props;
-    return <> {children}</>;
+  const { children } = props;
+  return <> {children}</>;
 };
-GoodsCenterTable.displayName = 'GoodsCenterTable';
+GoodsCenterTable.displayName = "GoodsCenterTable";
 
-/**
- * 商品表格
- */
-export default WithTable<WithTableProps>(GoodsCenterTable, pageConfig, (props, searchInfo, pagetInfo) => {
-
-    pagetInfo.changeCol('saleModel', {
-
-        render: (value, record, index) => {
-            const { saleUnit } = record
-            //if (params.)
-            //const [value,record,index,dictText]=
-            return `${saleUnit}(${value})`
-        }
+/** 商品表格 */
+export default WithTable<WithTableProps>(
+  GoodsCenterTable,
+  pageConfig,
+  (props, searchInfo, pagetInfo) => {
+    pagetInfo.changeCol("saleModel", {
+      render: (value, record, index) => {
+        const { saleUnit } = record;
+        //if (params.)
+        //const [value,record,index,dictText]=
+        return `${saleUnit}(${value})`;
+      },
     });
-    
-    searchInfo.changeSearchItem('goodsSearchKey', {
-        width: '630px',
+
+    searchInfo
+      .changeSearchItem("goodsSearchKey", {
+        width: 630,
       })
       .toConfig();
     return {
-        //request: defaultReq,
-        ...props,
-        bordered: false,
-        size: 'small',
-        rowKey: 'goodsId',
-        pageSize: 5,
-        //pagination: { pageSize: 5, pageSizeOptions: [5, 10, 20], current: 1 },
-        scroll: { y: 360 }
-       
-
-
-    }
-});
+      //request: defaultReq,
+      ...props,
+      bordered: false,
+      size: "small",
+      rowKey: "goodsId",
+      pageSize: 5,
+      //pagination: { pageSize: 5, pageSizeOptions: [5, 10, 20], current: 1 },
+      scroll: { y: 420, x: 560 },
+    };
+  }
+);
