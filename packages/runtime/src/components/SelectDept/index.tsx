70161327a1c31163f4db00795553c0d4b35b950d
chengtianhua <1057691751@qq.com>
2022-04-29 11:49:46 +0800

    选择机构页面样式修正

--- packages/runtime/src/components/SelectDept/index.tsx
+++ packages/runtime/src/components/SelectDept/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { getUser, updateCurrentDept } from '../Auth';
+import { getUser, updateCurrentDept, clearUser, DeptInfoProps } from '../Auth';
 import { uesRequest } from '../../utils/api';
 import logo from '../../assets/logo.svg';
+import Avatar from '../Layout/GlobalHeader/AvatarDropdown';
 // @ts-ignore
 import { history } from 'umi';
 import './index.less';
-import { message, Layout, Card } from 'antd';
+import { message, Layout, Card, List } from 'antd';
 const { Header, Content } = Layout;
 
 const SelectDept: React.FC<any> = (props) => {
   const user = getUser();
   const { run } = uesRequest('user', 'chooseDept');
+  const logout = uesRequest('user', 'logout');
   const selectOrg = async (deptId: any) => {
     let data = await run({ deptId });
     if (data) {
@@ -24,35 +26,70 @@ const SelectDept: React.FC<any> = (props) => {
       message.warn('找不到该组织');
     }
   };
+  const layoutFn = () => {
+    logout.run().then(() => {
+      clearUser();
+      history.push('/login');
+    });
+  };
 
   const renderDept = () => {
     if (user && user.optionalDepts) {
       const depList = user.optionalDepts;
       const currentDept = user.chooseDeptVO?.currentDept;
-      const itemsList = depList.map((val) => {
-        const { bizDeptId, bizDeptName, bizDeptTypeName } = val;
-        let className = 'org-list-item';
-        if (currentDept?.bizDeptId === bizDeptId) {
-          className = 'org-list-item action';
-        }
-        return (
-          <div
-            className={className}
-            key={bizDeptId}
-            title={bizDeptName}
-            onClick={() => {
-              selectOrg(bizDeptId);
-            }}
-          >
-            <div className="name">{bizDeptName}</div>
-            <div className="desc-item">机构类型:{bizDeptTypeName || ''}</div>
-          </div>
-        );
-      });
+      // const itemsList = depList.map((val) => {
+      //   const { bizDeptId, bizDeptName, bizDeptTypeName } = val;
+      //   let className = 'org-list-item';
+      //   if (currentDept?.bizDeptId === bizDeptId) {
+      //     className = 'org-list-item action';
+      //   }
+      //   return (
+      //     <div
+      //       className={className}
+      //       key={bizDeptId}
+      //       title={bizDeptName}
+      //       onClick={() => {
+      //         selectOrg(bizDeptId);
+      //       }}
+      //     >
+      //       <div className="name">{bizDeptName}</div>
+      //       <div className="desc-item">机构类型:{bizDeptTypeName || ''}</div>
+      //     </div>
+      //   );
+      // });
       return (
-        <Card title="机构列表" bordered={false}>
-          <div className="inner-content">{itemsList}</div>
-        </Card>
+        <List<DeptInfoProps>
+          rowKey="bizDeptId"
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 4,
+            xl: 6,
+            xxl: 6,
+          }}
+          dataSource={depList}
+          renderItem={(item) => (
+            <List.Item key={item.bizDeptId}>
+              <Card
+                className={
+                  currentDept?.bizDeptId === item.bizDeptId ? 'card-action' : ''
+                }
+                hoverable
+                bodyStyle={{ paddingBottom: 20 }}
+                onClick={() => {
+                  selectOrg(item.bizDeptId);
+                }}
+              >
+                <Card.Meta
+                  title={item.bizDeptName}
+                  description={`机构类型:${item.bizDeptTypeName || ''}`}
+                />
+              </Card>
+            </List.Item>
+          )}
+        />
       );
     }
     return <div className="inner-wrapper"></div>;
@@ -85,11 +122,13 @@ const SelectDept: React.FC<any> = (props) => {
               <img src={logo}></img>
               <h1>长嘴猫平台</h1>
             </div>
-            <div className="ant-pro-right-content">{user?.realName}</div>
+            <Avatar currentUser={user} menu={true} layoutFn={layoutFn} />
           </div>
         </Header>
         <Content className="ant-pro-basicLayout-content layout-select-content">
-          <div className="select-wrapper">{renderDept()}</div>
+          <Card title="机构列表">
+            <div className="select-wrapper">{renderDept()}</div>
+          </Card>
         </Content>
       </Layout>
     </div>
