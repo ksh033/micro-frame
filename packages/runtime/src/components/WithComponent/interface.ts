93abc2c12bcfba6f95fee4782de3dc7f23bc1009
chengtianhua <1057691751@qq.com>
2022-05-25 17:09:16 +0800

    修改引用

--- packages/runtime/src/components/WithComponent/interface.ts
+++ packages/runtime/src/components/WithComponent/interface.ts
@@ -1,8 +1,11 @@
-import type { BsEditTableProps } from '@/components/Base/BsEditTable';
-import type { BsTableProps } from '@/components/Base/BsTable';
+import type { BsEditTableProps } from '../Base/BsEditTable';
+import type { BsTableProps } from '../Base/BsTable';
 import type { CheckboxProps } from 'antd';
 //import type { RowSelectionType } from 'antd/es/table/interface';
-import type { FormSearchItem, ProColumn } from '@scboson/sc-schema/es/interface';
+import type {
+  FormSearchItem,
+  ProColumn,
+} from '@scboson/sc-schema/es/interface';
 import type { ScSelectProps } from '@scboson/sc-element/es/sc-select';
 import type { MutableRefObject } from 'react';
 
@@ -15,18 +18,15 @@ export type WithTableProps = {
   children?: any;
 
   formatPrams?: (params: any) => any;
-  getCheckboxProps?: (record: any) => Partial<Omit<CheckboxProps, 'defaultChecked' | 'checked'>>;
+  getCheckboxProps?: (
+    record: any
+  ) => Partial<Omit<any, 'defaultChecked' | 'checked'>>;
 } & BsTableProps;
 
-
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type WithSelectProps = {
-
-} & ScSelectProps
+export type WithSelectProps = {} & ScSelectProps;
 export type WithSelectTableProps = {
-  /**
-   * 显示类型
-   */
+  /** 显示类型 */
   type?: 'select' | 'table';
   buttonText?: string;
 
@@ -34,30 +34,20 @@ export type WithSelectTableProps = {
    * 选择中的表格数据转业务数据
    */
   getValueProps?: (rowDatas: any) => any;
-  /**
-   *业务数据转表格数据
-   */
+  /** 业务数据转表格数据 */
   normalize?: (value: any) => any;
-  /**
-   *  选择框确认事件
-   */
+  /** 选择框确认事件 */
   onOk?: (selecteds?: any[]) => any;
   textPropName?: string;
   // valuePropName?: string;
   value?: any;
 
-  /**
-   * 表单onChange
-   */
+  /** 表单onChange */
   onChange?: any;
-  /**
-   * 选中表格属性
-   */
+  /** 选中表格属性 */
   tableProps?: BsEditTableProps;
   className?: string;
-  /**
-   * 选择行
-   */
+  /** 选择行 */
   selectedRows?: any[];
   dataRef?: MutableRefObject<{ selectedRows?: any[]; selectedKeys?: any[] }>;
   disabled?: boolean;
