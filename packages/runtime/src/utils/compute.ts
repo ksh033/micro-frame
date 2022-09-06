e8f5d10f27a3a40da3c586c27db7d68e3c3a9bab
chengtianhua <1057691751@qq.com>
2022-09-06 17:22:49 +0800

    精度计算修正

--- packages/runtime/src/utils/compute.ts
+++ packages/runtime/src/utils/compute.ts
@@ -73,8 +73,20 @@ function operation(a: any, b: any, op: string) {
 
 // 加减乘除的四个接口
 // 加
-function add(a: any, b: any) {
-  return operation(a, b, 'add');
+function add(arg1, arg2) {
+  let r1, r2, m;
+  try {
+    r1 = arg1.toString().split('.')[1].length;
+  } catch (e) {
+    r1 = 0;
+  }
+  try {
+    r2 = arg2.toString().split('.')[1].length;
+  } catch (e) {
+    r2 = 0;
+  }
+  m = Math.pow(10, Math.max(r1, r2));
+  return (arg1 * m + arg2 * m) / m;
 }
 // 减
 function subtract(arg1, arg2) {
