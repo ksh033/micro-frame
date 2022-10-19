b8704ea6c59bece46e73cde71f2320304068e52e
chengtianhua <1057691751@qq.com>
2022-10-19 14:29:07 +0800

    runtime 添加BsTimePicker组件

--- packages/runtime/src/components/Base/index.ts
+++ packages/runtime/src/components/Base/index.ts
@@ -1,28 +1,30 @@
-import PageSchema from '@scboson/sc-schema'
-import DictSelect from './DictSelect'
-import BsPriceInput from './BsPriceInput'
-import BsNumberInput from './BsNumberInput'
-import BsUpload from './BsUpload'
-import BsImg from './BsImg'
-import BsVideo from './BsVideo'
-import BsViewImage from './BsViewImage'
-import FieldImageName from './FieldImageName'
-import Volume from './Volume'
-import BsRangePicker from './BsRangePicker'
-import BsRangeTimePicker from './BsRangeTimePicker'
-import BsIcon from './BsIcon'
-import BsEditTable from './BsEditTable'
+import PageSchema from '@scboson/sc-schema';
+import DictSelect from './DictSelect';
+import BsPriceInput from './BsPriceInput';
+import BsNumberInput from './BsNumberInput';
+import BsUpload from './BsUpload';
+import BsImg from './BsImg';
+import BsVideo from './BsVideo';
+import BsViewImage from './BsViewImage';
+import FieldImageName from './FieldImageName';
+import Volume from './Volume';
+import BsRangePicker from './BsRangePicker';
+import BsRangeTimePicker from './BsRangeTimePicker';
+import BsIcon from './BsIcon';
+import BsEditTable from './BsEditTable';
+import BsTimePicker from './BsTimePicker';
 
-const { regeditCmp } = PageSchema
-regeditCmp('DictSelect', DictSelect)
-regeditCmp('BsUpload', BsUpload)
-regeditCmp('FieldImageName', FieldImageName)
-regeditCmp('BsNumberInput', BsNumberInput)
-regeditCmp('BsPriceInput', BsPriceInput)
-regeditCmp('Volume', Volume)
-regeditCmp('BsRangePicker', BsRangePicker)
-regeditCmp('BsRangeTimePicker', BsRangeTimePicker)
-regeditCmp('BsEditTable', BsEditTable)
+const { regeditCmp } = PageSchema;
+regeditCmp('DictSelect', DictSelect);
+regeditCmp('BsUpload', BsUpload);
+regeditCmp('FieldImageName', FieldImageName);
+regeditCmp('BsNumberInput', BsNumberInput);
+regeditCmp('BsPriceInput', BsPriceInput);
+regeditCmp('Volume', Volume);
+regeditCmp('BsRangePicker', BsRangePicker);
+regeditCmp('BsRangeTimePicker', BsRangeTimePicker);
+regeditCmp('BsEditTable', BsEditTable);
+regeditCmp('BsTimePicker', BsTimePicker);
 
 export {
   DictSelect,
@@ -38,4 +40,4 @@ export {
   BsRangeTimePicker,
   BsIcon,
   BsEditTable,
-}
+};
