1278991f428b6e17523444109436322d6269648e
chengtianhua <1057691751@qq.com>
2022-12-06 14:38:53 +0800

    添加应用

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -27,6 +27,7 @@ export const qiankun = new Promise((resolve) => {
     { systemCode: 'financesys', systemName: '财务应用' },
     { systemCode: 'bisys', systemName: '数据中心' },
     { systemCode: 'deliverysys', systemName: '配送应用' },
+    { systemCode: 'cookbooksys', systemName: '菜品应用' },
   ];
   const apps: any[] = [];
   const routes: any[] = [];
