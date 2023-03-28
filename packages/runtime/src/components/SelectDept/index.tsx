13137b90386d279d8c6e55f7162c822874475307
chengtianhua <1057691751@qq.com>
2023-03-28 09:49:13 +0800

    机构选择修改为分组模式

--- packages/runtime/src/components/SelectDept/index.tsx
+++ packages/runtime/src/components/SelectDept/index.tsx
@@ -4,15 +4,26 @@ import { uesRequest } from '../../utils/api';
 // @ts-ignore
 import { history } from 'umi';
 import './index.less';
-import { message, Card, List, Badge } from 'antd';
+import { message, Card, List, Badge, Divider } from 'antd';
 import NotMenuLayouy from '../Layout/NoMenuLayout';
+
+// 排序列表
+const sort = [
+  'COMPANY', // 集团
+  'SUPPLY_SUBCOMPANY', // 子公司
+  'SUPPLY_CHAIN_COMPANY', // 供应链公司
+  'WAREHOUSE', // 仓库
+  'CHAIN_MANAGE_COMPANY', // 连锁管理公司
+  'SHOP', // 门店
+  'STATION ', // 站点
+];
 
 const SelectDept: React.FC<any> = (props) => {
   const user = getUser();
   const { run } = uesRequest('user', 'chooseDept');
   const deptlistApi = uesRequest('user', 'deptlist');
 
-  const [userDeptlist, setDeptlist] = useState<any[]>([]);
+  const [userDeptlist, setDeptlist] = useState<DeptInfoProps[]>([]);
 
   useEffect(() => {
     deptlistApi.run().then((res) => {
@@ -43,76 +54,191 @@ const SelectDept: React.FC<any> = (props) => {
       const depList =
         userDeptlist.length > 0 ? userDeptlist : user.optionalDepts;
       const currentDept = user.chooseDeptVO?.currentDept;
+      console.log('depList', depList);
+
+      const groupMap: Record<
+        string,
+        { bizDeptTypeName: string; list: DeptInfoProps[] }
+      > = {};
+
+      if (Array.isArray(depList)) {
+        depList.forEach((item) => {
+          if (groupMap[item.bizDeptType]) {
+            const list = groupMap[item.bizDeptType].list;
+            list.push(item);
+            groupMap[item.bizDeptType] = {
+              bizDeptTypeName: item.bizDeptTypeName,
+              list: list,
+            };
+          } else {
+            groupMap[item.bizDeptType] = {
+              bizDeptTypeName: item.bizDeptTypeName,
+              list: [item],
+            };
+          }
+        });
+      }
+      const length = Object.keys(groupMap).length;
+      const width = Math.round(95 / length);
       return (
-        <List<DeptInfoProps>
-          rowKey="bizDeptId"
-          grid={{
-            gutter: 16,
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 4,
-            xl: 6,
-            xxl: 6,
-          }}
-          dataSource={depList}
-          renderItem={(item) => (
-            <List.Item key={item.bizDeptId}>
-              {Number(item.todoNumber || 0) > 0 ? (
-                <Badge.Ribbon
-                  text={`${Number(item.todoNumber || 0)}`}
-                  color="#cf1322"
+        <>
+          {sort.map((bizDeptType: string) => {
+            if (groupMap[bizDeptType]) {
+              return (
+                <div
+                  key={bizDeptType}
+                  className="select-wrapper-item"
+                  style={{ width: width + '%' }}
                 >
-                  <Card
-                    className={
-                      currentDept?.bizDeptId === item.bizDeptId
-                        ? 'card-action'
-                        : ''
+                  <Badge
+                    status="processing"
+                    text={
+                      <span style={{ fontSize: '16px', fontWeight: 500 }}>
+                        {groupMap[bizDeptType].bizDeptTypeName}
+                      </span>
                     }
-                    hoverable
-                    bodyStyle={{ paddingBottom: 20 }}
-                    onClick={() => {
-                      selectOrg(item.bizDeptId);
-                    }}
-                  >
-                    <Card.Meta
-                      title={item.bizDeptName}
-                      description={`机构类型:${item.bizDeptTypeName || ''}`}
-                    />
-                  </Card>
-                </Badge.Ribbon>
-              ) : (
-                <Card
-                  className={
-                    currentDept?.bizDeptId === item.bizDeptId
-                      ? 'card-action'
-                      : ''
-                  }
-                  hoverable
-                  bodyStyle={{ paddingBottom: 20 }}
-                  onClick={() => {
-                    selectOrg(item.bizDeptId);
-                  }}
-                >
-                  <Card.Meta
-                    title={item.bizDeptName}
-                    description={`机构类型:${item.bizDeptTypeName || ''}`}
+                    style={{ marginBottom: '12px', marginLeft: '12px' }}
                   />
-                </Card>
-              )}
-            </List.Item>
-          )}
-        />
+                  <List<DeptInfoProps>
+                    rowKey="bizDeptId"
+                    grid={{
+                      gutter: 16,
+                      xs: 1,
+                      sm: 1,
+                      md: 1,
+                      lg: 1,
+                      xl: 1,
+                      xxl: 1,
+                    }}
+                    dataSource={groupMap[bizDeptType].list}
+                    renderItem={(item) => (
+                      <List.Item key={item.bizDeptId}>
+                        {Number(item.todoNumber || 0) > 0 ? (
+                          <Badge.Ribbon
+                            text={`${Number(item.todoNumber || 0)}`}
+                            color="#cf1322"
+                          >
+                            <Card
+                              className={
+                                currentDept?.bizDeptId === item.bizDeptId
+                                  ? 'card-action'
+                                  : ''
+                              }
+                              hoverable
+                              bodyStyle={{ padding: 18 }}
+                              onClick={() => {
+                                selectOrg(item.bizDeptId);
+                              }}
+                            >
+                              <Card.Meta
+                                title={item.bizDeptName}
+                                // description={`机构类型:${
+                                //   item.bizDeptTypeName || ''
+                                // }`}
+                              />
+                            </Card>
+                          </Badge.Ribbon>
+                        ) : (
+                          <Card
+                            className={
+                              currentDept?.bizDeptId === item.bizDeptId
+                                ? 'card-action'
+                                : ''
+                            }
+                            hoverable
+                            bodyStyle={{ padding: 18 }}
+                            onClick={() => {
+                              selectOrg(item.bizDeptId);
+                            }}
+                          >
+                            <Card.Meta
+                              title={item.bizDeptName}
+                              // description={`机构类型:${
+                              //   item.bizDeptTypeName || ''
+                              // }`}
+                            />
+                          </Card>
+                        )}
+                      </List.Item>
+                    )}
+                  />
+                </div>
+              );
+            }
+            return null;
+          })}
+        </>
       );
+      // return (
+      //   <List<DeptInfoProps>
+      //     rowKey="bizDeptId"
+      //     grid={{
+      //       gutter: 16,
+      //       xs: 1,
+      //       sm: 2,
+      //       md: 3,
+      //       lg: 4,
+      //       xl: 6,
+      //       xxl: 6,
+      //     }}
+      //     dataSource={depList}
+      //     renderItem={(item) => (
+      //       <List.Item key={item.bizDeptId}>
+      //         {Number(item.todoNumber || 0) > 0 ? (
+      //           <Badge.Ribbon
+      //             text={`${Number(item.todoNumber || 0)}`}
+      //             color="#cf1322"
+      //           >
+      //             <Card
+      //               className={
+      //                 currentDept?.bizDeptId === item.bizDeptId
+      //                   ? 'card-action'
+      //                   : ''
+      //               }
+      //               hoverable
+      //               bodyStyle={{ paddingBottom: 20 }}
+      //               onClick={() => {
+      //                 selectOrg(item.bizDeptId);
+      //               }}
+      //             >
+      //               <Card.Meta
+      //                 title={item.bizDeptName}
+      //                 description={`机构类型:${item.bizDeptTypeName || ''}`}
+      //               />
+      //             </Card>
+      //           </Badge.Ribbon>
+      //         ) : (
+      //           <Card
+      //             className={
+      //               currentDept?.bizDeptId === item.bizDeptId
+      //                 ? 'card-action'
+      //                 : ''
+      //             }
+      //             hoverable
+      //             bodyStyle={{ paddingBottom: 20 }}
+      //             onClick={() => {
+      //               selectOrg(item.bizDeptId);
+      //             }}
+      //           >
+      //             <Card.Meta
+      //               title={item.bizDeptName}
+      //               description={`机构类型:${item.bizDeptTypeName || ''}`}
+      //             />
+      //           </Card>
+      //         )}
+      //       </List.Item>
+      //     )}
+      //   />
+      // );
     }
     return <div className="inner-wrapper"></div>;
   };
 
   return (
     <NotMenuLayouy>
-      <Card title="机构列表">
-        <div className="select-wrapper">{renderDept()}</div>
-      </Card>
+      {/* <Card title="机构列表" bodyStyle={{ padding: '0 24px' }}> */}
+      <div className="select-wrapper">{renderDept()}</div>
+      {/* </Card> */}
     </NotMenuLayouy>
   );
 };
